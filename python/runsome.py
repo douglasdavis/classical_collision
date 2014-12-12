ce3fa1a74ec6f7b509699acff8fabaa8fb6a5622
Douglas Davis <drd25@phy.duke.edu>
2014-12-11 20:01:43 -0500

    moving to lninux

--- python/runsome.py
+++ python/runsome.py
@@ -2,7 +2,9 @@
 
 import subprocess
 
+default_settings='--p-radius 1 --t-radius 2 --p-mass 0.25 --t-mass 2 -s 1'
+
 cur = .5
 for i in xrange(100):
-    subprocess.call("./main -c 6 -l "+str(cur)+" >> data.dat",shell=True)
+    subprocess.call("./main "+str(default_settings)+" -l "+str(cur)+" >> data.dat",shell=True)
     cur = cur + 0.025
