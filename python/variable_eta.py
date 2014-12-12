e76a973f73a4db9f3030f449f37f0ca7a34b7e4e
Douglas Davis <drd25@phy.duke.edu>
2014-12-11 22:57:30 -0500

    ya

--- python/variable_eta.py
+++ python/variable_eta.py
@@ -7,20 +7,20 @@ import sys
 data = np.loadtxt(sys.argv[1])
 tm, tr, pm, pr, dt, pvx0, tvx0, ip, k, eta, L, ms, fvxt, fvyt, fvt, fvxp, fvyp, fvp, tts = data.T
 
-dx_vs_ip = ROOT.TGraph()
-ms_vs_ip = ROOT.TGraph()
+dx_vs_eta = ROOT.TGraph()
+ms_vs_eta = ROOT.TGraph()
 i = 0
-for m,n in zip(ms,ip):
-    dx_vs_ip.SetPoint(i,n,tr[0]+pr[0]-m)
-    ms_vs_ip.SetPoint(i,n,m)
+for m,n in zip(ms,eta):
+    dx_vs_eta.SetPoint(i,n,tr[0]+pr[0]-m)
+    ms_vs_eta.SetPoint(i,n,m)
     i = i + 1
 
-dx_vs_ip.SetMarkerStyle(7)
-ms_vs_ip.SetMarkerStyle(7)
-ms_vs_ip.SetMarkerColor(ROOT.kRed)
+dx_vs_eta.SetMarkerStyle(7)
+ms_vs_eta.SetMarkerStyle(7)
+ms_vs_eta.SetMarkerColor(ROOT.kRed)
 
 mg = ROOT.TMultiGraph()
-mg.Add(dx_vs_ip)
+mg.Add(dx_vs_eta)
 
 mg.Draw('AP')
 
