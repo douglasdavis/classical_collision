c0f2043d9ffdf955d2f3db3dbde734d3e7bdd7f4
Douglas Davis <drd25@phy.duke.edu>
2014-12-11 20:40:29 -0500

    leaving the office time to go home

--- python/runsome.py
+++ python/runsome.py
@@ -2,9 +2,9 @@
 
 import subprocess
 
-default_settings='--p-radius 1 --t-radius 2 --p-mass 0.25 --t-mass 2 -s 1'
+default_settings='--p-radius 1 --t-radius 2 --p-mass 0.25 --t-mass 2 --delta-t .0001'
 
-cur = .5
+cur = 0
 for i in xrange(100):
-    subprocess.call("./main "+str(default_settings)+" -l "+str(cur)+" >> data.dat",shell=True)
-    cur = cur + 0.025
+    subprocess.call("./main "+str(default_settings)+" -s "+str(cur)+" >> data.dat",shell=True)
+    cur = cur+.02
